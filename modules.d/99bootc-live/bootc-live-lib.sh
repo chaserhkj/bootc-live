f78e5ad97dbde423a01e7297890fbee989f0376d
Chaser Huang <huangkangjing@gmail.com>
2026-01-10 17:14:11 -0500

    use proper logging instead of writing directly to console

--- modules.d/99bootc-live/bootc-live-lib.sh
+++ modules.d/99bootc-live/bootc-live-lib.sh
@@ -1,13 +1,16 @@
 # Redirecting output to console
 # Mainly for monitoring long-running processes like image fetching and unpacking
-bootc_redirect_to_console() {
-    "$@" 2>&1 | tee /dev/console
+
+type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
+
+bootc_warn_output() {
+    "$@" 2>&1 | vwarn
 }
 
 check_bootc_quiet() {
     if getargbool 0 bootc.quiet; then
         bootc_long_running_process=
     else
-        bootc_long_running_process=bootc_redirect_to_console
+        bootc_long_running_process=bootc_warn_output
     fi
 }
